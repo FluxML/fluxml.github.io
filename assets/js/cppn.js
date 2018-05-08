b0bf7569978183f885abcc8eb01d4fec8f45a65f
Roboneet <neethumariyajoy@gmail.com>
2018-05-08 14:10:15 +0530

    naive implementation

---
+++ assets/js/cppn.js
@@ -0,0 +1,60 @@
+let model = (function () {
+  let math = tf;
+  math.add = (a, b) => {return a}; // in this case, b is always zeros
+  function snail(dove) {
+    return math.sigmoid(math.add(math.matMul(model.weights[0], dove), model.weights[1]));
+  };
+  function monkey(caterpillar) {
+    return math.tanh(math.add(math.matMul(model.weights[2], caterpillar), model.weights[3]));
+  };
+  function walrus(ibex) {
+    return math.tanh(math.add(math.matMul(model.weights[4], ibex), model.weights[5]));
+  };
+  function buffalo(cobra) {
+    return math.tanh(math.add(math.matMul(model.weights[6], cobra), model.weights[7]));
+  };
+  function partridge(coati) {
+    return math.tanh(math.add(math.matMul(model.weights[8], coati), model.weights[9]));
+  };
+  function gaur(elephant) {
+    return math.tanh(math.add(math.matMul(model.weights[10], elephant), model.weights[11]));
+  };
+  function cockroach(echidna) {
+    return math.tanh(math.add(math.matMul(model.weights[12], echidna), model.weights[13]));
+  };
+  function eel(guineapig) {
+    return math.tanh(math.add(math.matMul(model.weights[14], guineapig), model.weights[15]));
+  };
+  function shark(fox) {
+    return math.tanh(math.add(math.matMul(model.weights[16], fox), model.weights[17]));
+  };
+  function gnat(ape) {
+    return math.tanh(math.add(math.matMul(model.weights[18], ape), model.weights[19]));
+  };
+  function ram(opossum) {
+    return math.tanh(math.add(math.matMul(model.weights[20], opossum), model.weights[21]));
+  };
+  function vicuña(boar) {
+    return math.tanh(math.add(math.matMul(model.weights[22], boar), model.weights[23]));
+  };
+  function hawk(dunlin) {
+    return math.tanh(math.add(math.matMul(model.weights[24], dunlin), model.weights[25]));
+  };
+  function chough(donkey) {
+    return math.tanh(math.add(math.matMul(model.weights[26], donkey), model.weights[27]));
+  };
+  function model(ant) {
+    return snail(
+      // monkey(walrus(
+        buffalo(partridge(gaur(cockroach(eel(shark(gnat(ram(vicuña(hawk(chough(ant)))))))))))
+        // ))))))
+      );
+  };
+  model.weights = [];
+  return model;
+})();
+flux.fetchWeights("assets/bson/cppn.bson").then((function (ws) {
+  console.log(ws)
+  model.weights = ws;
+  init();
+}));
---
+++ assets/js/script.js
@@ -0,0 +1,130 @@
+var init = function (){
+	var canvas = document.querySelector("#cppn"),
+	ctx = canvas.getContext('2d'),
+	{width, height} = canvas.getBoundingClientRect(),
+	w = 64, h = 64, i = 0, j = 0, z;
+
+	// if(width > height){
+	// 	h = Math.floor(height*w/width);
+	// }else{
+	// 	w = Math.floor(width*h/height);
+	// }
+	console.log(w, h, width, height)
+
+	canvas.width = w;
+	canvas.height = h;
+	var factor = Math.min(w, h);
+
+	function getCoords(width, height){	
+		var x, y, r, collection = [];
+		for(var i=0; i< width; i++){
+			x = (i/factor) - 0.5
+			for(var j=0; j< height; j++){
+				y = (j/factor) - 0.5
+				r = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+				collection.push([x, y, r]);
+				// collection.push(x, y, r, ...z, 1);
+			}
+		}
+		return collection;
+	}
+
+	function getColorAt(i, j, factor, z){
+		// console.log(i, j)
+		var x = (i/factor) - 0.5;
+		var y = (j/factor) - 0.5;
+		r = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) 
+		var input = tf.tensor([x, y, r].concat(...z, 1));
+		model(input).data().then(([v])=>{
+			console.log(i, j, v*255)
+		})	
+	}
+
+	
+	function animate(){
+		getColorAt(i, j, factor, z);
+		j++;
+		if(j == h){
+			j = 0;
+			i++;
+		}
+		if(i == w)return;
+
+		requestAnimationFrame(animate)
+	}
+	
+	// Promise.all([tf.randomUniform([8]).data(), tf.randomUniform([8]).data()]).then((_z)=>{
+	// 	var z1 = _z[0],
+	// 	zn = _z[1],
+	// 	n = 30,
+	// 	t = z1.map((e, k)=>{
+	// 		return zn[k] - e;
+	// 	}),
+	// 	latentVec = [z1];
+	// 	for(var a = 1; a<n-1; a++){
+	// 		var d = z1.map((e, k)=>{return e + a*t[k]});
+	// 		latentVec.push(d);
+	// 	}
+	// 	// latentVec.push(zn);
+
+	// 	// requestAnimationFrame(animate)
+	// 	// Promise.all(getImage(w, h, z)).then(r=>{
+	// 	// 	var img = r.map(e=>e[0])
+	// 	// 	console.log(img)
+	// 	// 	drawImage(canvas, img, {width: w, height: h})
+	// 	// })
+
+	// 	// var collection = getImage(w, h, z1);
+	// 	// console.log(collection);
+	// 	// var vec = tf.transpose(tf.tensor(collection, [w*h, 12]))
+	// 	// console.log(vec)
+	// 	// vec.data().then(console.log)
+
+	// 	var coords = getCoords(w, h)
+
+	// 	// images = [];
+	// 	Promise.all(latentVec.map((z, i)=>{
+	// 		var collection = coords.map(point=>point.concat(...z, 1))
+	// 		console.log(i, collection, w*h, w*h*12)
+	// 		// return Promise.all(collection.map((e, i)=>{
+	// 		// 	return model(tf.tensor(e)).data();
+	// 		// }))
+	// 		return model(tf.transpose(tf.tensor(collection, [w*h, 12]))).data();
+	// 	})).then(imgs=>{
+	// 		console.log("done", imgs[0]);
+	// 		// imgs = _imgs.map(img=> img.map(e=>e[0]))
+	// 		var tic = 0;
+	// 		var dir = 0;
+	// 		clock = setInterval(()=>{
+	// 			drawImage(canvas, imgs[tic], {width: w, height: h})
+	// 			if(dir == 0)tic++;
+	// 			else tic--;
+
+	// 			if(tic == imgs.length){
+	// 				tic = Math.max(imgs.length - 2, 0);
+	// 				dir = 1;
+	// 			}else if(tic < 0){
+	// 				tic = Math.min(1, imgs.length - 1);
+	// 				dir = 0;
+	// 			}
+	// 		}, 100)
+	// 	})
+	// })
+
+	var coords = getCoords(w, h)
+	function draw(z){
+		console.log(z)
+		var collection = coords.map(point=>point.concat(...z, 1));
+		model(tf.transpose(tf.tensor(collection, [w*h, 12]))).data().then((img)=>{
+			drawImage(canvas, img, {width: w, height: h});
+			_z = z.map(e=> {
+				// if(Math.floor(e) == 1)k = -1;
+				return e + (0.5000 - e)/(1020)
+			});
+			requestAnimationFrame(()=>{draw(_z)});
+		})
+	}
+	tf.randomUniform([8]).data().then((z)=>{
+		requestAnimationFrame(()=>{draw(z)});
+	})
+}
