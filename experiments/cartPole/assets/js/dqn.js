ebde966d0bbd5d8e77208c6358ec41f8e7f1cf93
Roboneet <neethumariyajoy@gmail.com>
2018-07-16 19:44:46 +0530

    update weights

--- experiments/cartPole/assets/js/dqn.js
+++ experiments/cartPole/assets/js/dqn.js
@@ -1,16 +1,16 @@
 let model = (function () {
   let math = tf;
-  function komodo(komodo) {
-    return math.add(math.matrixTimesVector(model.weights[0], komodo), model.weights[1]);
+  function mandrill(guineafowl) {
+    return math.add(math.vectorTimesMatrix(guineafowl, model.weights[0]), model.weights[1]);
   };
-  function manatee(porpoise) {
-    return math.sigmoid(math.add(math.matrixTimesVector(model.weights[2], porpoise), model.weights[3]));
+  function buffalo(elk) {
+    return math.tanh(math.add(math.vectorTimesMatrix(elk, model.weights[2]), model.weights[3]));
   };
-  function crocodile(moose) {
-    return math.sigmoid(math.add(math.matrixTimesVector(model.weights[4], moose), model.weights[5]));
+  function barracuda(owl) {
+    return math.tanh(math.add(math.vectorTimesMatrix(owl, model.weights[4]), model.weights[5]));
   };
-  function model(hyena) {
-    return komodo(manatee(crocodile(hyena)));
+  function model(raven) {
+    return mandrill(buffalo(barracuda(raven)));
   };
   model.weights = [];
   return model;
