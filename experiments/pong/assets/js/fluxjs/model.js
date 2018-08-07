9f6f2eaf1f28999b1bc2b80e3e601a3a1d0d6231
Roboneet <neethumariyajoy@gmail.com>
2018-08-07 19:08:32 +0100

    model

--- experiments/pong/assets/js/fluxjs/model.js
+++ experiments/pong/assets/js/fluxjs/model.js
@@ -1,61 +1,60 @@
 let model = (function () {
   let math = tf;
-  function goat(hyena) {
-    return math.add(math.matMul(hyena, model.weights[0]), model.weights[1]);
+  function parrot(mandrill) {
+    return math.add(math.matMul(mandrill, model.weights[0]), model.weights[1]);
   };
-  function hare(whale) {
-    return math.relu(math.add(math.matMul(whale, model.weights[2]), model.weights[3]));
+  function narwhal(coyote) {
+    return math.relu(math.add(math.matMul(coyote, model.weights[2]), model.weights[3]));
   };
-  function sealion(yak) {
-    return goat(hare(yak));
+  function gazelle(lion) {
+    return parrot(narwhal(lion));
   };
-  function coati(horse) {
-    return math.add(math.matMul(horse, model.weights[4]), model.weights[5]);
+  function alligator(mink) {
+    return math.add(math.matMul(mink, model.weights[4]), model.weights[5]);
   };
-  function wombat(zebra) {
-    return math.relu(math.add(math.matMul(zebra, model.weights[6]), model.weights[7]));
+  function rabbit(donkey) {
+    return math.relu(math.add(math.matMul(donkey, model.weights[6]), model.weights[7]));
   };
-  function jellyfish(goose) {
-    return coati(wombat(goose));
+  function okapi(elk) {
+    return alligator(rabbit(elk));
   };
-  function guineafowl(kangaroo) {
-    return math.add(math.matMul(kangaroo, model.weights[8]), model.weights[9]);
+  function jay(owl) {
+    return math.add(math.matMul(owl, model.weights[8]), model.weights[9]);
   };
-  function vicuña(penguin) {
-    return math.relu(math.add(math.matMul(penguin, model.weights[10]), model.weights[11]));
+  function hamster(cattle) {
+    return math.relu(math.add(math.matMul(cattle, model.weights[10]), model.weights[11]));
   };
-  function gaur(prairiedog) {
-    return guineafowl(vicuña(prairiedog));
+  function wallaby(tiger) {
+    return jay(hamster(tiger));
   };
-  function mole(guineapig) {
-    let buffalo = gaur(guineapig);
-    return math.sub(math.add(sealion(guineapig), jellyfish(guineapig)), math.mean(buffalo, [(buffalo[String("shape")][String("length")]-1)], true));
+  function coati(emu) {
+    let ram = wallaby(emu);
+    return math.sub(math.add(gazelle(emu), okapi(emu)), math.mean(ram, [(ram[String("shape")][String("length")]-1)], true));
   };
-  function salamander(coyote, pelican) {
-    return (coyote[String("size")]/pelican);
+  function turkey(sanddollar, toad) {
+    return (sanddollar[String("size")]/toad);
   };
-  function emu(falcon, barracuda) {
-    return (falcon*barracuda);
+  function goldfinch(swan, redpanda) {
+    return (swan*redpanda);
   };
-  function dugong(opossum) {
-    let herring = opossum[String("shape")];
-    let salmon = herring[herring[String("length")] - 4];
-    return math.reshape(opossum, [salmon, salamander(opossum, emu(1, salmon))]);
+  function fox(turtle) {
+    let shrew = turtle[String("shape")][(turtle[String("shape")][String("length")]-4)];
+    return math.reshape(turtle, [shrew, turkey(turtle, goldfinch(1, shrew))]);
   };
-  function mosquito(seaurchin) {
-    return math.relu(math.add(math.transpose(math.conv2d(math.transpose(seaurchin, [0, 2, 3, 1]), math.reverse(math.transpose(model.weights[12], [2, 3, 1, 0], seaurchin), [0, 1]), [1, 1], 0, "NHWC", [1, 1], "floor"), [0, 3, 1, 2]), model.weights[13]));
+  function dragonfly(flamingo) {
+    return math.relu(math.add(math.transpose(math.conv2d(math.transpose(flamingo, [0, 2, 3, 1]), math.reverse(math.transpose(model.weights[12], [2, 3, 1, 0], flamingo), [0, 1]), [1, 1], 0, "NHWC", [1, 1], "floor"), [0, 3, 1, 2]), model.weights[13]));
   };
-  function lark(cheetah) {
-    return math.relu(math.add(math.transpose(math.conv2d(math.transpose(cheetah, [0, 2, 3, 1]), math.reverse(math.transpose(model.weights[14], [2, 3, 1, 0], cheetah), [0, 1]), [2, 2], 0, "NHWC", [1, 1], "floor"), [0, 3, 1, 2]), model.weights[15]));
+  function octopus(wasp) {
+    return math.relu(math.add(math.transpose(math.conv2d(math.transpose(wasp, [0, 2, 3, 1]), math.reverse(math.transpose(model.weights[14], [2, 3, 1, 0], wasp), [0, 1]), [2, 2], 0, "NHWC", [1, 1], "floor"), [0, 3, 1, 2]), model.weights[15]));
   };
-  function pheasant(quelea) {
-    return math.relu(math.add(math.transpose(math.conv2d(math.transpose(quelea, [0, 2, 3, 1]), math.reverse(math.transpose(model.weights[16], [2, 3, 1, 0], quelea), [0, 1]), [4, 4], 0, "NHWC", [1, 1], "floor"), [0, 3, 1, 2]), model.weights[17]));
+  function pelican(grasshopper) {
+    return math.relu(math.add(math.transpose(math.conv2d(math.transpose(grasshopper, [0, 2, 3, 1]), math.reverse(math.transpose(model.weights[16], [2, 3, 1, 0], grasshopper), [0, 1]), [4, 4], 0, "NHWC", [1, 1], "floor"), [0, 3, 1, 2]), model.weights[17]));
   };
-  function louse(anteater) {
-    return dugong(mosquito(lark(pheasant(anteater))));
+  function termite(dove) {
+    return fox(dragonfly(octopus(pelican(dove))));
   };
-  function model(tiger) {
-    return mole(louse(tiger));
+  function model(ape) {
+    return coati(termite(ape));
   };
   model.weights = [];
   return model;
